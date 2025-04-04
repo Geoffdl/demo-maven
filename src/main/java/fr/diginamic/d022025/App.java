3b620f00cde780435b52a5b85dbba484acb1dfc1
Geoffdl <geoffroydelorme@gmail.com>
2025-04-04 14:41:07 +0200

    + asciiArt

--- src/main/java/fr/diginamic/d022025/App.java
+++ src/main/java/fr/diginamic/d022025/App.java
@@ -1,10 +1,24 @@
 package fr.diginamic.d022025;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        try
+        {
+            String asciiArt = FigletFont.convertOneLine("Welcome to Demo-Maven");
+            System.out.println(asciiArt);
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
+
 }
